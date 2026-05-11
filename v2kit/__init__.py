6472e85eb989c83ff2cee6e6398a9dee18c8bd85
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-05-11 18:38:15 +0330

    feat : add v2kit folder

---
+++ v2kit/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""v2kit modules."""
+from .params import V2KIT_VERSION
+__version__ = V2KIT_VERSION
+
+__all__ = []
---
+++ v2kit/params.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""v2kit params."""
+
+V2KIT_VERSION = "0.1"
