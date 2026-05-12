b5e9efead1846980b965887bf8fd21c84c404ee4
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-05-12 18:28:44 +0330

    fix : fix minor bug in __init__

--- v2kit/__init__.py
+++ v2kit/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """v2kit modules."""
-from .params import V2KIT_VERSION
+from .params import V2KIT_VERSION, Protocol
 from .functions import is_vmess, is_vless, is_trojan, is_ss
 from .functions import relabel
 from .functions import encode_subscription, decode_subscription
 __version__ = V2KIT_VERSION
 
-__all__ = ["is_vmess", "is_vless", "is_trojan", "is_ss", "relabel", "encode_subscription", "decode_subscription"]
+__all__ = ["is_vmess", "is_vless", "is_trojan", "is_ss", "relabel", "encode_subscription", "decode_subscription", "Protocol"]
