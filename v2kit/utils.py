df9184edd9f02aee545b8c4e010d43e60aa685cb
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-05-27 16:21:44 +0330

    feat : add parse errors

--- v2kit/utils.py
+++ v2kit/utils.py
@@ -4,7 +4,7 @@ import json
 import base64
 from typing import Iterable
 from urllib.parse import urlparse
-from .errors import V2kitValidationError
+from .errors import V2kitValidationError, V2kitParseError
 from .validators import _validate_label
 from .params import DEFAULT_ENCODING
 from .params import Protocol
@@ -56,14 +56,14 @@ def _validate_config(uri: str) -> None:
         raise V2kitValidationError("Config cannot be empty.")
 
     if "://" not in uri:
-        raise V2kitValidationError("Invalid config format.")
+        raise V2kitParseError("Invalid config format.")
 
     parsed = urlparse(uri)
 
     try:
         Protocol(parsed.scheme)
     except Exception as exc:
-        raise V2kitValidationError(
+        raise V2kitParseError(
             f"Unsupported protocol: {parsed.scheme}"
         ) from exc
 
@@ -81,7 +81,7 @@ def _validate_config(uri: str) -> None:
                 raise ValueError
 
         except Exception as exc:
-            raise V2kitValidationError(
+            raise V2kitParseError(
                 "Invalid VMESS config."
             ) from exc
 
