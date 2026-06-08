51c4c83aad168287d767535ad6b5dfcc5a1615e1
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-06-08 17:40:23 +0330

    fix : add test_shadowsocks.py

---
+++ tests/test_shadowsocks.py
@@ -0,0 +1,15 @@
+from v2kit import parse
+from v2kit import ShadowsocksConfig
+
+def test_to_uri_roundtrip():
+    config = ShadowsocksConfig(
+        encryption="aes-256-gcm",
+        password="password",
+        address="example.com",
+        port=8388,
+        label="test",
+    )
+
+    parsed = parse(config.to_uri())
+
+    assert parsed == config
