faa177d14483450cfaa19d2129b04141e66c59ea
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-06-20 16:49:38 +0330

    fix : add test_to_dict

--- tests/test_vmess.py
+++ tests/test_vmess.py
@@ -17,6 +17,29 @@ def test_defaults():
     assert config.extra == {}
 
 
+def test_to_dict():
+    config = VMESSConfig(
+        uuid="1c4b4bca-e3ff-4ca8-a062-6f399ad3cf45",
+        address="example.com",
+        port=443,
+        alter_id=1,
+        network="ws",
+        tls="tls",
+        label="test",
+    )
+
+    assert config.to_dict() == {
+        "v": "2",
+        "ps": "test",
+        "add": "example.com",
+        "port": "443",
+        "id": "1c4b4bca-e3ff-4ca8-a062-6f399ad3cf45",
+        "aid": "1",
+        "net": "ws",
+        "tls": "tls",
+    }
+
+
 def test_to_uri_roundtrip():
     config = VMESSConfig(
         uuid="1c4b4bca-e3ff-4ca8-a062-6f399ad3cf45",
