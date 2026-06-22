d4592416d3758c3dba1844cfbe3be834adf47e2c
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-06-22 15:09:21 +0330

    fix : add test_defaults

--- tests/test_trojan.py
+++ tests/test_trojan.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import pytest
 from v2kit import parse
 from v2kit import TrojanConfig
+
+def test_defaults():
+    config = TrojanConfig(
+        password="password",
+        address="example.com",
+        port=443,
+    )
+
+    assert config.label is None
+    assert config.extra == {}
 
 
 def test_to_uri_roundtrip():
